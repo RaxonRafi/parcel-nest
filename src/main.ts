190497405f8891cea4cf258a71a45ddeb7478f3c
Rafi <itzmeh320@gmail.com>
2026-05-28 20:41:18 +0600

    vercel Settings

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UserService } from './user/user.service';
 
+let app;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const userService = app.get(UserService);
+  if (app) return app;
 
+  app = await NestFactory.create(AppModule);
+
+  const userService = app.get(UserService);
   await userService.seedSuperAdmin();
 
   app.setGlobalPrefix('api');
@@ -14,11 +17,18 @@ async function bootstrap() {
     origin: process.env.CORS_ORIGIN?.split(',') ?? [
       'http://localhost:3001',
       'http://127.0.0.1:3001',
-      'https://percel-client-next.vercel.app'
-      
+      'https://percel-client-next.vercel.app',
     ],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.init(); // ✅ init() instead of listen()
+  return app;
 }
-bootstrap();
+
+// ✅ Required default export for Vercel
+export default async (req: any, res: any) => {
+  const server = await bootstrap();
+  const httpAdapter = server.getHttpAdapter().getInstance();
+  httpAdapter(req, res);
+};
